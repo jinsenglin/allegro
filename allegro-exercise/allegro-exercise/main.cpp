c7c2138b654aac35e56658dcbfabe174d06acb5b
Jim Lin <jimlintw922@gmail.com>
2018-01-14 11:57:24 +0800

    use 128x128 image for character

--- allegro-exercise/allegro-exercise/main.cpp
+++ allegro-exercise/allegro-exercise/main.cpp
@@ -221,19 +221,19 @@ int process_event(){
             // P1 control
             case ALLEGRO_KEY_W:
                 if (character1.y - 30 >= 0) character1.y -= 30;
-                if (!character1WeaponFlying) character1Weapon.y = character1.y + 50;
+                if (!character1WeaponFlying) character1Weapon.y = character1.y + 64;
                 break;
             case ALLEGRO_KEY_S:
                 if (character1.y + 30 <= HEIGHT-100) character1.y += 30;
-                if (!character1WeaponFlying) character1Weapon.y = character1.y + 50;
+                if (!character1WeaponFlying) character1Weapon.y = character1.y + 64;
                 break;
             case ALLEGRO_KEY_A:
                 if (character1.x - 30 >= 0) character1.x -= 30;
-                if (!character1WeaponFlying) character1Weapon.x = character1.x + 50;
+                if (!character1WeaponFlying) character1Weapon.x = character1.x + 64;
                 break;
             case ALLEGRO_KEY_D:
                 if (character1.x + 30 <= WIDTH-100) character1.x += 30;
-                if (!character1WeaponFlying) character1Weapon.x = character1.x + 50;
+                if (!character1WeaponFlying) character1Weapon.x = character1.x + 64;
                 break;
             case ALLEGRO_KEY_X:
                 if (!character1WeaponFlying) character1WeaponFlying = true;
@@ -242,19 +242,19 @@ int process_event(){
             // P2 control
             case ALLEGRO_KEY_UP:
                 if (character2.y - 30 >= 0) character2.y -= 30;
-                if (!character2WeaponFlying) character2Weapon.y = character2.y + 50;
+                if (!character2WeaponFlying) character2Weapon.y = character2.y + 64;
                 break;
             case ALLEGRO_KEY_DOWN:
                 if (character2.y + 30 <= HEIGHT-100) character2.y += 30;
-                if (!character2WeaponFlying) character2Weapon.y = character2.y + 50;
+                if (!character2WeaponFlying) character2Weapon.y = character2.y + 64;
                 break;
             case ALLEGRO_KEY_RIGHT:
                 if (character2.x + 30 <= WIDTH-100) character2.x += 30;
-                if (!character2WeaponFlying) character2Weapon.x = character2.x + 50;
+                if (!character2WeaponFlying) character2Weapon.x = character2.x + 64;
                 break;
             case ALLEGRO_KEY_LEFT:
                 if (character2.x - 30 >= 0) character2.x -= 30;
-                if (!character2WeaponFlying) character2Weapon.x = character2.x + 50;
+                if (!character2WeaponFlying) character2Weapon.x = character2.x + 64;
                 break;
             case ALLEGRO_KEY_Y:
                 if (!character2WeaponFlying) character2WeaponFlying = true;
@@ -493,21 +493,20 @@ void setup_characters() {
     character1.x = WIDTH / 2 - 300;
     character1.y = HEIGHT / 2 + 75;
     character2.x = WIDTH / 2 + 170;
-    character2.y = HEIGHT / 2 + 100;
-    character1.image_path = al_load_bitmap("maokai.png");
-    character2.image_path= al_load_bitmap("teemo.png");
-    character3.image_path = al_load_bitmap("Azir.png");
+    character2.y = HEIGHT / 2 + 75;
+    character1.image_path = al_load_bitmap("dragon.png");
+    character2.image_path= al_load_bitmap("ninja.png");
     background = al_load_bitmap("stage.jpg");
     
     character1HP = 100;
     character2HP = 100;
     
     // Setting Character's Weapon
-    character1Weapon.x = character1.x + 50;
-    character1Weapon.y = character1.y + 50;
+    character1Weapon.x = character1.x + 64;
+    character1Weapon.y = character1.y + 64;
     character1Weapon.image_path = al_load_bitmap("fireball.png");
-    character2Weapon.x = character2.x + 50;
-    character2Weapon.y = character2.y + 50;
+    character2Weapon.x = character2.x + 64;
+    character2Weapon.y = character2.y + 64;
     character2Weapon.image_path = al_load_bitmap("dart.png");
 }
 
